abedf30022eddcad5b257048b57f29df5ac5f40d
RomanPie2020 <romanstone2004@gmail.com>
2020-01-14 16:36:38 +0200

    first

---
+++ main.js
@@ -0,0 +1,11 @@
+// Adaptive menu
+$(document).ready(function () {
+    $(".burger").click(function () {
+        if ($(".mobile_menu_links").is(":hidden")) {
+            $(".mobile_menu_links").show("fast");
+        } else {
+            $(".mobile_menu_links").hide("");
+        }
+        return false;
+    });
+});
