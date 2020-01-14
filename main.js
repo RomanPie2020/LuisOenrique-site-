8a23978dd6963403425cbcbe7ef1d7cf831f7978
RomanPie2020 <romanstone2004@gmail.com>
2020-01-14 16:53:27 +0200

    second

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ $(document).ready(function () {
         if ($(".mobile_menu_links").is(":hidden")) {
             $(".mobile_menu_links").show("fast");
         } else {
-            $(".mobile_menu_links").hide("");
+            $(".mobile_menu_links").hide("fast");
         }
         return false;
     });
