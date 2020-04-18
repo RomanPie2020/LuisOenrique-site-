afcc8c3390ca3b201cfff02b3fdf19a0ef63f677
RomanPie2020 <romanstone2004@gmail.com>
2020-04-18 13:08:43 +0300

    18.04.2020

--- main.js
+++ main.js
@@ -1,19 +1,42 @@
 // Adaptive menu
-$(document).ready(function () {
-    $(".burger").click(function () {
-        if ($(".mobile_menu_links").is(":hidden")) {
-            $(".mobile_menu_links").show("fast");
-        } else {
-            $(".mobile_menu_links").hide("fast");
-        }
-        return false;
-    });
-});
-// testimonials slider 
-$(document).ready(function () {
-    $('.single_item').slick({
-        arrows: false,
-        autoplay: true,
-        dots: true,
-    });
-});
+let a = document.querySelector('.burger');
+let b = document.querySelector('.mobile_menu');
+let c = document.querySelectorAll('.mobile_menu_link')[0];
+let d = document.querySelectorAll('.mobile_menu_link')[1];
+let e = document.querySelectorAll('.mobile_menu_link')[2];
+let f = document.querySelectorAll('.mobile_menu_link')[3];
+let g = document.querySelectorAll('.mobile_menu_link')[4];
+a.onclick = function () {
+    
+    if (b.style.display == "none") {
+        b.style.display = "flex";
+    }
+    else{
+        b.style.display = "none";
+    }
+}
+c.onclick = function () {
+
+    b.style.display = "none";
+
+}
+d.onclick = function () {
+    
+    b.style.display = "none";
+    
+}
+e.onclick = function () {
+
+    b.style.display = "none";
+
+}
+f.onclick = function () {
+
+    b.style.display = "none";
+
+}
+g.onclick = function () {
+
+    b.style.display = "none";
+
+}
