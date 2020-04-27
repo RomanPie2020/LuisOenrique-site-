e716dea352cfe36d19a0f1d53bf54ee181114b79
Roman <59442135+RomanPie2020@users.noreply.github.com>
2020-04-27 13:25:33 +0300

    Update main.js

--- main.js
+++ main.js
@@ -6,15 +6,14 @@ let d = document.querySelectorAll('.mobile_menu_link')[1];
 let e = document.querySelectorAll('.mobile_menu_link')[2];
 let f = document.querySelectorAll('.mobile_menu_link')[3];
 let g = document.querySelectorAll('.mobile_menu_link')[4];
-a.onclick = function () {
+$(document).ready(function () {
+    a.click(function (event) {
+        b.toggleClass('active');
+        
+        
+    });
     
-    if (b.style.display == "flex";) {
-        b.style.display = "none"
-    }
-    else{
-         b.style.display = "flex";
-    }
-}
+});
 c.onclick = function () {
 
     b.style.display = "none";
